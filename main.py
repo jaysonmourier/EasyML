6562dfaac032348e8c34c05cc6a643611ad604a5
cdemiribel <clement.demiribel@gmail.com>
2023-03-20 15:35:09 +0100

    USE

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pandas as pd
 from textx import metamodel_from_file
 
 easyml_meta = metamodel_from_file('easyml.tx')
-easy_model = easyml_meta.model_from_str('main.easyml')
+easy_model = easyml_meta.model_from_file('main.easyml')
 
-df = pd.read_csv(easy_model.name, skipinitialspace=True, usecols= easy_model.names)
+a =  easy_model.fichier.name
+
+
+print(a)
