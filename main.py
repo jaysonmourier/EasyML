ef02bddd43884c850bd262a9479e58dd5c119fd7
cdemiribel <clement.demiribel@gmail.com>
2023-03-20 17:02:11 +0100

    evol_TextX_Fonctionnelle

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+# from textx import metamodel_from_file
+
+# easyml_meta = metamodel_from_file('easyml.tx')
+# easy_model = easyml_meta.model_from_file('main.easyml')
+
+# a =  easy_model.fichier.name
+
+
+# print(a)
+
+
 from textx import metamodel_from_file
+from textx.scoping.providers import RelativeName, FQN
+from textx.export import model_export
+# ------------------------------------
+# GRAMMAR
+#
+meta_model = metamodel_from_file('easyml.tx')
 
-easyml_meta = metamodel_from_file('easyml.tx')
-easy_model = easyml_meta.model_from_file('main.easyml')
 
-a =  easy_model.fichier.name
+# ------------------------------------
+# EXAMPLE
+#
+model = meta_model.model_from_file('main.easyml')
 
+a= model.Fichiers.name+".csv"
 
+print(model.Cibles.name)
+print(model.Standardizers.state)
+print(model.Modeles.name)
 print(a)
