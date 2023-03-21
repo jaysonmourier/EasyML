5c6c356d40dc0a074bc906a0403c0a128bd193fa
cdemiribel <clement.demiribel@gmail.com>
2023-03-21 17:01:54 +0100

    TextX fin

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from textx import metamodel_from_file
 from textx.scoping.providers import RelativeName, FQN
 from textx.export import model_export
-
+import regl
+from sklearn.svm import SVC
 
 meta_model = metamodel_from_file('easyml.tx')
 
 
 model = meta_model.model_from_file('main.easyml')
 
-a= model.Fichiers.name+".csv"
 
-print(model.Cibles.name)
-print(model.Standardizers.state)
-print(model.Modeles.name)
-print(a)
+file= model.Fichiers.name+".csv"
+target = model.Cibles.name
+std = model.Standardizers.state
+modeluse = model.Modeles.name
+features = []
+
+for i in model.Features.names:
+    features.append(i.name) ## nom des colonnes
+
+
+print("le fichier sélectionné est : "+file)
+print("\n les colonnes utilisées sont : ")
+print(features)
+
+if(std):
+    print("\n l'expresion linéaire sera standardisé ")
+else:
+    print("\n l'expresion linéaire ne sera pas standardisé ")
+
+print("la cible est : "+target)
+
+
+
+# mo = regl.train(file,features,target,std,SVC(gamma='auto'))
+# print(mo.predict([[10, 123]]))
