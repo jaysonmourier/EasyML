bfab532940979ab72bd1c625c3572261c26409b6
Jayson Mourier <mourierjayson@protonmail.com>
2023-05-19 12:10:56 +0200

    fix

--- easyml/pool.py
+++ easyml/pool.py
@@ -6,7 +6,6 @@ class Pool:
 
     def __init__(self, test_size, *models):
         self.models = models
-        self.__load_models()
 
     def __load_models(self):
         for model in self.models:
